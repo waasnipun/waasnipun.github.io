e3ce62af68af2bbba5020b5baa37799bd2420224
Nipun Waas <nipun@yaalalabs.com>
2023-02-05 12:49:24 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ed105e77d92eed5fe298.js"
+    "url": "webpack-runtime-503df39f3b943c0d1cc8.js"
   },
   {
     "url": "framework-b2d6e508d75a672689cc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "91917d48ccfd50ad3bd807f24e9a5e83"
+    "revision": "254f15153e3cd76d4b3972adc467e57d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-27c66d5dcd0cba7d37a1.js"
