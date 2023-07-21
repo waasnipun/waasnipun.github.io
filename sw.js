175ea96ea40dea4181300b7f4870d5789003b66e
waasnipun <waasnipun@gmail.com>
2023-07-21 15:52:47 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8d410540301b5db996e0.js"
+    "url": "webpack-runtime-7aa4294f9913e9d21b61.js"
   },
   {
     "url": "framework-b2d6e508d75a672689cc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b6bd2b69d5024f1f1a7a803e4a1108a0"
+    "revision": "d449f1a0196cda8bd430048f5b7cc6ee"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-27c66d5dcd0cba7d37a1.js"
